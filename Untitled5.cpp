51ae606bebb92fd4de8ea315e14ff2214b2174cb
vienkkk <165811539+vienkkk@users.noreply.github.com>
2024-12-19 08:39:27 +0700

    Add files via upload

--- Untitled5.cpp
+++ Untitled5.cpp
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
 int main() {
+	int i;
 	int a[5]= {1,2,3,4,5};
-	printf("so be nhat %d\n",a[0]);
-	printf("so lon nhat %d\n",a[4]);
+	int min=a[0];
+	for(i=0; i<5; i++) {
+		if(min>a[i]) {
+			min=a[i];
 
+		}
+	}
+	printf("so be nhat %d\n",min);
+	int max=a[0];
+	for(i=0; i<5; i++) {
+		if(max<a[i]) {
+			max=a[i];
+		}
+	}
+	printf("so lon nhat %d\n",max);
 	return 0;
 }
